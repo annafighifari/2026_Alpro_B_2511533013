34fec0101900e87befee25104f4400950723ba93
DashG <annafighifari@gmail.com>
2026-09-03 10:16:39 +0700

    git config --global user.name "DashG" git config --global user.email "annafighifari@gmail.com"

---
+++ Pekan 1/program1_2511533013.py
@@ -0,0 +1,12 @@
+# program pertama
+"""
+Komentar 
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("Program ini menghasilkan")
+print("Empat baris output")
+print("/ \\ \\\\ /// \\\\\\")
+print("program ini" + " dibuat oleh a/n " + "Annafi Al Ghifari dan 2511533013.")
